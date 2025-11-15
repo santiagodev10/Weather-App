a49d68612445e7a18cffa2077bdcc4969db3c07f
santiagodev <sanxp18@gmail.com>
2025-11-15 13:35:27 -0400

    setup of the project: organize folder structure and files based on the purpuse that corresponds to it, create some base files like global.css and variables.css to add the css reset, load the necessary fonts for the project, and css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 style={{fontSize: "3rem", fontFamily: "Bricolage Grotesque", textAlign: "center"}}>Weather App</h1>
+      <h2 style={{fontSize: "2rem", fontFamily: "DM Sans", textAlign: "center"}}>Welcome to your Weather App built with React!</h2>
     </>
   )
 }
