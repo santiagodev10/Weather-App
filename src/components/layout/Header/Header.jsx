976308780e724f7205d24ce330117100b807ecfe
santiagodev <sanxp18@gmail.com>
2025-11-15 23:42:04 -0400

    add DropdownButton to select units and toggle metric/imperial options, with visual feedback to the user

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -7,8 +7,12 @@ const Header = () => {
         <header className={styles['header-container']}>
             <img className={styles.logo} src={Logo} alt="Weather App Logo" />
             <DropdownButton 
-                options={['Celsius', 'Fahrenheit']} 
-                onSelect={(option) => console.log(`Selected temperature unit: ${option}`)} 
+                temperature={['Celsius (°C)', 'Fahrenheit (°F)']} 
+                onSelectTemp={(temperatureSelect) => console.log(`Selected temperature unit: ${temperatureSelect}`)}
+                windSpeed={["km/h", "mph"]}
+                onSelectWind={(windSelect) => console.log(`Selected wind speed unit: ${windSelect}`)}
+                precipitation={["Millimeters (mm)", "Inches (in)"]}
+                onSelectPrep={(precipitationSelect) => console.log(`Selected precipitation unit: ${precipitationSelect}`)}
             />
         </header>
     )
--- src/components/sections/DropdownButton/DropdownButton.jsx
+++ src/components/sections/DropdownButton/DropdownButton.jsx
@@ -3,31 +3,75 @@ import iconDropdown from "../../../assets/images/icon-dropdown.svg";
 import iconUnits from "../../../assets/images/icon-units.svg";
 import styles from "./DropdownButton.module.css";
 
-const DropdownButton = ({ options, onSelect }) => {
+const DropdownButton = ({ temperature, onSelectTemp, windSpeed, onSelectWind, precipitation, onSelectPrep }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isImperial, setIsImperial] = useState("Switch to Imperial");
+    const [selectedTemp, setSelectedTemp] = useState(null);
+    const [selectedWind, setSelectedWind] = useState(null);
+    const [selectedPrep, setSelectedPrep] = useState(null);
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
 
-    const handleSelect = (option) => {
-        onSelect(option);
-        setIsOpen(false);
+    const handleSelectTemp = (option) => {
+        onSelectTemp(option);
+        setSelectedTemp(option);
     };
+
+    const handleSelectWind = (option) => {
+        onSelectWind(option);
+        setSelectedWind(option);
+    }
+
+    const handleSelectPrep = (option) => {
+        onSelectPrep(option);
+        setSelectedPrep(option);
+    }
+
+    const switchUnitSystem = () => {
+        if (isImperial === "Switch to Imperial") {
+            setIsImperial("Switch to Metric");
+            handleSelectTemp("Fahrenheit (°F)");
+            handleSelectWind("mph");
+            handleSelectPrep("Inches (in)");
+        } else {
+            setIsImperial("Switch to Imperial");
+            handleSelectTemp("Celsius (°C)");
+            handleSelectWind("km/h");
+            handleSelectPrep("Millimeters (mm)");
+        }
+    }
 
     return (
         <div className={styles["dropdown-container"]}>
             <button className={styles["dropdown-button"]} onClick={toggleDropdown} type="button">
                 <img src={iconUnits} alt="Units Icon" />
                 Units
-                <img src={iconDropdown} alt="Dropdown Icon" />
+                <img className={`${styles["dropdown-icon"]} ${isOpen ? styles.rotate : ""}`} src={iconDropdown} alt="Dropdown Icon" />            
             </button>
+            
             {isOpen && (
                 <ul className={styles["dropdown-menu"]}>
-                    {options.map((option, index) => (
-                        <li key={index} onClick={() => handleSelect(option)} className="dropdown-item">
-                            {option}
-                        </li>
+                    <button className={`${styles["switch-unit-system"]} ${styles["dropdown-item"]}`} onClick={switchUnitSystem}>{isImperial}</button>
+
+                    <h2 className={styles["dropdown-label"]}>Temperature</h2>
+                    {temperature.map((option, index) => (
+                        <li key={index} onClick={() => handleSelectTemp(option)} className={`${styles["dropdown-item"]} ${selectedTemp === option ? styles["dropdown-item-selected"] : ""}`}>{option}</li>
+                    ))}
+
+                    <span className={styles.divisor}></span>
+
+                    <h2 className={styles["dropdown-label"]}>Wind Speed</h2>
+                    {windSpeed.map((option, index) => (
+                        <li key={index} onClick={() => handleSelectWind(option)} className={`${styles["dropdown-item"]} ${selectedWind === option ? styles["dropdown-item-selected"] : ""}`}>{option}</li>
+                    ))}
+
+                    <span className={styles.divisor}></span>
+
+                    <h2 className={styles["dropdown-label"]}>Precipitation</h2>
+                    {precipitation.map((option, index) => (
+                        <li key={index} onClick={() => handleSelectPrep(option)} className={`${styles["dropdown-item"]} ${selectedPrep === option ? styles["dropdown-item-selected"] : ""}`}>{option}</li>
                     ))}
                 </ul>
             )}
